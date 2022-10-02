6079ff07f7ea16a6b07c8dc414b7d4c814cdcc6b
Luis Acosta <acosta1395@hotmail.com>
2022-10-02 20:46:29 -0300

    finished

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,8 +6,11 @@ export default function NavBar(){
     return(
         <div>
             <div className="NavBar">
+                
                 <div>
                     <img className='Logo' src='https://www.pngplay.com/wp-content/uploads/12/Fornite-Black-Knight-PNG-Images-HD.png' alt='Knight'/>
+                </div>
+                <div>    
                     <h2 className='LinkHome'>LuishiGames</h2>
                 </div>
 
